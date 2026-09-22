c21c948c3a5ec2bac20325595ffbcded213e6224
teranmaleesha <teranmaleesha@gmail.com>
2026-09-22 12:49:02 +0530

    Update energy.py

--- energy.py
+++ energy.py
@@ -1,1 +1,25 @@
+PLANCK_CONSTANT = 6.626e-34
 
+
+def main():
+    while True:
+        try:
+            frequency = float(input("Enter Frequency: "))
+            if frequency > 0:
+                break
+            else:
+                print("Frequency must be positive!")
+        except ValueError:
+            print("Invalid input, enter a number! ")
+
+    energy = calculate_energy(frequency)
+    print(f"Energy: {energy:.2e} Joules")
+
+
+def calculate_energy(frequency):
+    E = PLANCK_CONSTANT * frequency
+    return E
+
+
+if __name__ == "__main__":
+    main()
